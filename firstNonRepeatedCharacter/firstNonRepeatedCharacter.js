5948ef2d95d90799172e5bec2c6ee04c85219b34
seif-miehiar <seif.miehiar@gmail.com>
2020-02-20 22:30:08 +0100

    added firstNonRepeatedCharacter toy problem

---
+++ firstNonRepeatedCharacter/firstNonRepeatedCharacter.js
@@ -0,0 +1,7 @@
+// firstNonRepeatedCharacter
+// Given an arbitrary input string, return the first non-repeating character. 
+//For strings with all repeats, return 'sorry'
+function firstNonRepeatedCharacter(string) {
+	//type your code here
+
+}
