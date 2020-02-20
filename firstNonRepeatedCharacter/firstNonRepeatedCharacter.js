871f65ea0b4774639a315fa001c2b5f4e4731c2e
seif-miehiar <seif.miehiar@gmail.com>
2020-02-20 22:43:29 +0100

    solved - fixed and documented

--- firstNonRepeatedCharacter/firstNonRepeatedCharacter.js
+++ firstNonRepeatedCharacter/firstNonRepeatedCharacter.js
@@ -2,21 +2,29 @@
 // Given an arbitrary input string, return the first non-repeating character. 
 //For strings with all repeats, return 'sorry'
 function firstNonRepeatedCharacter(string) {
-	//type your code here
+	//declare an empty object variable
 	let storage = {};
-	let array = string.split(' ');
+	//splitting the string inot an array each character will be an value in one index
+	let array = string.split('');
 
-	for (let i = 0; i < array; i++) {
-		if (storage[array[i]] !== undefined) {
+	//for loop over the array of strings
+	for (let i = 0; i < array.length; i++) {
+		//checking if the object has the value as an key/prop
+		if (storage[array[i]] === undefined) {
+			//if not assign it with a value of 1
 			storage[array[i]] = 1
 		} else {
+			//if not just increment that value
 			storage[array[i]]++
 		}
-		for (let key in storage) {
-			if (storage[key] === 1) {
-				return key;
-			}
+	}
+	//loop over the object 
+	for (let key in storage) {
+		//checking if a value of the key/prop equals 1
+		if (storage[key] === 1) {
+			return key;
 		}
 	}
+	//if no return statment were excuted return "sorry"
 	return 'sorry'
 }
