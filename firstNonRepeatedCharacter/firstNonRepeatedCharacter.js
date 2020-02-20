38b3721bd7c9c1d7384b67e8700ecae327ceee01
seif-miehiar <seif.miehiar@gmail.com>
2020-02-20 22:33:40 +0100

    testing code

--- firstNonRepeatedCharacter/firstNonRepeatedCharacter.js
+++ firstNonRepeatedCharacter/firstNonRepeatedCharacter.js
@@ -3,5 +3,20 @@
 //For strings with all repeats, return 'sorry'
 function firstNonRepeatedCharacter(string) {
 	//type your code here
+	let storage = {};
+	let array = string.split(' ');
 
+	for (let i = 0; i < array; i++) {
+		if (storage[array[i]] !== undefined) {
+			storage[array[i]] = 1
+		} else {
+			storage[array[i]]++
+		}
+		for (let key in storage) {
+			if (storage[key] === 1) {
+				return key;
+			}
+		}
+	}
+	return 'sorry'
 }
