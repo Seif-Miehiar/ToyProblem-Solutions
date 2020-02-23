6116536593420bbd66f132833e660bc5725233c4
seif-miehiar <seif.miehiar@gmail.com>
2020-02-23 23:42:44 +0100

    Solved

--- onlyUnique/onlyUnique.js
+++ onlyUnique/onlyUnique.js
@@ -1,2 +1,20 @@
 // Only Unique
 // Given a string, remove any characters that are not unique from the string.
+function onlyUnique(str) {
+  let storage = {};
+  let string = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (storage[str[i]] === undefined) {
+      storage[str[i]] = 1;
+    } else {
+      storage[str[i]]++;
+    }
+  }
+  for (let key in storage) {
+    if (storage[key] === 1) {
+      string += key;
+    }
+  }
+  return string;
+}
