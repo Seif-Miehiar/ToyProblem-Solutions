a943ed228f02ba9d58e6be2c67041aecb29ed457
seif-miehiar <seif.miehiar@gmail.com>
2020-02-24 21:55:40 +0100

    Solved

--- balancedParenthesis/balancedParenthesis.js
+++ balancedParenthesis/balancedParenthesis.js
@@ -2,5 +2,12 @@
 // Given a string, return true if it contains balanced parenthesis ().
 
 function isBalanced(string) {
+  let counter = 0;
 
+  for (let char of string) {
+    if (counter < 0) return false;
+    if (char === '(') counter++;
+    if (char === ')') counter--;
+  }
+  return counter === 0 ? true : false;
 }
